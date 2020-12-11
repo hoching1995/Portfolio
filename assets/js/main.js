3043fc45448ca192950a1130e9cb27829fa47c41
hochinl <hochinl@wwu.edu>
2020-12-10 19:20:43 -0800

    updated home_img

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title', {});
 sr.reveal('.button', { delay: 200 });
-sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__img', { delay: 200 });
 sr.reveal('.home__social-icon', { interval: 200 });
 
 /*SCROLL ABOUT*/
